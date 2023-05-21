4002e794f2a1c0aa467dfa8d1d2d24bba344b8ed
Chaz Lakinger <clakinger@gmail.com>
2023-05-20 20:40:18 -0500

    allow for downloading posts with multiple images

--- bstage_downloader.py
+++ bstage_downloader.py
@@ -7,27 +7,33 @@ import subprocess
 def download_photo(response):
     if response.status_code != 200:
         exit(1)
-    image_url = str(response.content).split("\"mainImage\":\"")[1].split("\"")[0]
+
     post_id = str(response.text).split("\"post\":{\"id\":\"")[1].split("\"")[0]
-    media_id = image_url.split("/")[-2]
     author = str(response.text).split("\"nickname\":\"")[1].split("\"")[0]
 
     print(post_id)
-    print(media_id)
     print(author)
-    print(image_url)
 
-    output_folder = f"downloads/{author}/{post_id}" \
-        .format(author=author, post_id=post_id)
-    os.makedirs(output_folder, exist_ok=True)
-    full_path = output_folder + f"/{media_id}.jpeg".format(media_id=media_id)
-    print(full_path)
+    image_urls = str(response.content).split("\"images\":[")[1].split("]")[0]
+    for image_url in image_urls.split(","):
+        image_url = image_url.split("\"")[1]
+        media_id = image_url.split("/")[-2]
+        
+        print(image_url)
+        print(media_id)
+        
+        output_folder = f"downloads/{author}/{post_id}" \
+            .format(author=author, post_id=post_id)
+        os.makedirs(output_folder, exist_ok=True)
+        full_path = output_folder + f"/{media_id}.jpeg".format(media_id=media_id)
+        print(full_path)
 
-    image_response = requests.get(image_url)
-    if image_response.status_code != 200:
-        exit(1)
-    with open(full_path, 'wb') as file:
-        file.write(image_response.content)
+        image_response = requests.get(image_url)
+        if image_response.status_code != 200:
+            exit(1)
+        with open(full_path, 'wb') as file:
+            file.write(image_response.content)
+            file.close()
 
 
 def download_video(response):
